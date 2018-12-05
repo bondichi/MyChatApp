3ef5d8cfe6fddb84f7f553456759699f7715c420
Andrew Pisani <bondichi@gmail.com>
2018-12-06 09:46:03 +1100

    Completed layout xml for create account activity page

---
+++ app/src/main/java/com/bondi/mychatapp/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.bondi.mychatapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
